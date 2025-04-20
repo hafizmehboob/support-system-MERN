eed3944f31e4b7d9051006a54d6ed208488fce16
hafizmehboob <hafizabubakar.mehboob@gmail.com>
2025-04-20 18:28:53 +0500

    created .env file and configre express and added route to test in the postman, if route is working

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-console.log('Server....');
+const express = require('express')
+const dotenv = require('dotenv').config()
+
+const PORT = process.env.PORT || 8000  
+
+
+const app = express()
+
+app.get('/', (req, res) => {
+    // res.send('Gotcha')
+    res.status(200).json({message: 'welcome to support API',
+        body: 'Learn React JS'
+    })
+})
+
+app.listen(PORT, ()=> console.log(`Server started on the port ${PORT}`))
