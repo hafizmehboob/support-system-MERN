716f06212c28f6184f611a9ae553a420e8bea9c0
hafizmehboob <hafizabubakar.mehboob@gmail.com>
2025-04-23 17:24:19 +0500

    completed the user login controller and created token using JWT package to protect our routes which means user with token will access our routes

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,6 +1,6 @@
 const asyncHandler = require('express-async-handler') // for Auto error handling otherwise we need to write errors in trycatch
 const bcrypt = require('bcryptjs') // for passwords encryption
-
+const jwt = require('jsonwebtoken')
 const User = require('../models/userModels') // imported the user table
 // @desc Register a new user
 // @route /api/users
@@ -31,28 +31,53 @@ const registerUser = asyncHandler(async(req, res) => {
         email, 
         password: hashedPassword
     })
-
+    // 201 indicates that something is created
     if(user){
         res.status(201, {
             _id: user._id, // this is how mongo DB create user ID
             name: user.name,
             email: user.email,
+            token: generateToken(user._id)
         })
     }else{
         res.status(400)
         throw new Error('Invalid user data')
     }
-    
+
 })
 
 
 // @desc Login a user
 // @route /api/users/login
 // @access public
-const loginUser = asyncHandler(async(req, res) => {
-    res.send('Login Route');
-})
+    const loginUser = asyncHandler(async(req, res) => {
 
+        const {email, password} = req.body
+
+        const user = await User.findOne({email}) // finding user email into DB
+        // check user and passwords match
+        if(user && (await bcrypt.compare(password, user.password))){ // Matching the user password with DB password
+            res.status(200, {
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                token: generateToken(user._id)
+
+            })
+        }else{
+            res.status(401)
+            throw new Error('Invalid Credentials')
+        }
+
+    })
+
+// Generate Token
+// id = user._id
+const generateToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET, {
+        expiresIn: '30d' // expire days
+    })
+}
 
 module.exports = {
     registerUser,
