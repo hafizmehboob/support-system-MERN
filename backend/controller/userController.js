d5cc88dcf1e13e1f6ed623e10b418c4ccffaa9c5
hafizmehboob <hafizabubakar.mehboob@gmail.com>
2025-04-23 15:58:44 +0500

    completed the register user API and wrote code in the controller of registerUser

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,4 +1,7 @@
-const asyncHandler = require('express-async-handler') 
+const asyncHandler = require('express-async-handler') // for Auto error handling otherwise we need to write errors in trycatch
+const bcrypt = require('bcryptjs') // for passwords encryption
+
+const User = require('../models/userModels') // imported the user table
 // @desc Register a new user
 // @route /api/users
 // @access Public
@@ -10,7 +13,36 @@ const registerUser = asyncHandler(async(req, res) => {
         res.status(400)
         throw new Error('Please include all the fields')
     }
-    res.send('Register Route');
+
+    // Find if user already exist
+    const userExist = await User.findOne({email})
+    if(userExist){
+        res.status(400)
+        throw new Error('User already exist')
+    }
+
+    // Hash Password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // create User
+    const user = await User.create({
+        name,
+        email, 
+        password: hashedPassword
+    })
+
+    if(user){
+        res.status(201, {
+            _id: user._id, // this is how mongo DB create user ID
+            name: user.name,
+            email: user.email,
+        })
+    }else{
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+    
 })
 
 
