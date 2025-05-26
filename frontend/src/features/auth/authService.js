7261f1fc176c7808ad9a588a51afb06ee74559a6
hafizmehboob <hafizabubakar.mehboob@gmail.com>
2025-05-26 23:15:59 +0500

    Worked on logout functionality.

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -11,8 +11,11 @@ const register = async (userData) => {
     }
     return response.data;
 }
+// Logout
+const logout = () => localStorage.removeItem('user');
 
 const authService = {
-    register
+    register,
+    logout
 }
 export default authService
